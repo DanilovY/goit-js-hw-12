5cbe6867183b6da3b55d905e81465f914a51d7ed
Yurii Danilov <danilovyuriiii@gmail.com>
2024-12-14 15:53:45 +0200

    add

--- src/main.js
+++ src/main.js
@@ -23,67 +23,54 @@ let page = 1;
 form.addEventListener('submit', handSub);
 moreBtn.addEventListener('click', handMore);
 
-function handSub(event) {
+async function handSub(event) {
   event.preventDefault();
   inputValue = event.target.elements.input.value.trim();
   page = 1;
 
+  gallery.innerHTML = '';
+  loader.classList.remove('visually-hidden');
+  moreBtn.classList.add('visually-hidden');
+
   if (inputValue === '') {
     loader.classList.add('visually-hidden');
-    moreBtn.classList.add('visually-hidden');
     iziToast.warning({
       message: 'The field is empty!',
     });
     return;
   }
 
-  getImages(inputValue, page)
-    .then(response => {
-      if (response.total === 0) {
-        gallery.innerHTML = '';
-        throw new Error();
-      }
-      loader.classList.add('visually-hidden');
-      moreBtn.classList.remove('visually-hidden');
-      return response;
-    })
+  try {
+    const response = await getImages(inputValue, page);
+    loader.classList.add('visually-hidden');
 
-    .then(response => {
-      if (response.total <= 15) {
-        gallery.innerHTML = '';
-        gallery.innerHTML = creatImg(response.hits);
-        galleryImg.refresh();
-        loader.classList.add('visually-hidden');
-        moreBtn.classList.add('visually-hidden');
-        setTimeout(() => {
-          iziToast.warning({
-            message:
-              "We're sorry, but you've reached the end of search results.",
-          });
-        }, 1500);
-      }
-      if (response.total > 15) {
-        gallery.innerHTML = '';
-        gallery.innerHTML = creatImg(response.hits);
-        galleryImg.refresh();
-        loader.classList.add('visually-hidden');
-        moreBtn.classList.remove('visually-hidden');
-      }
-    })
-    .catch(() => {
-      loader.classList.add('visually-hidden');
-      moreBtn.classList.add('visually-hidden');
+    if (response.hits.length === 0) {
       iziToast.error({
-        iconColor: '#ffffff',
-        messageColor: '#ffffff',
-        progressBarColor: '#ffffff',
         message:
           'Sorry, there are no images matching your search query. Please try again!',
       });
-    })
-    .finally(() => {
-      event.target.elements.input.value = '';
+      return;
+    }
+
+    gallery.innerHTML = creatImg(response.hits);
+    galleryImg.refresh();
+
+    if (response.totalHits > 15) {
+      moreBtn.classList.remove('visually-hidden');
+    } else {
+      iziToast.warning({
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+  } catch (error) {
+    loader.classList.add('visually-hidden');
+    iziToast.error({
+      message:
+        'Sorry, there was an error fetching the images. Please try again!',
     });
+  } finally {
+    event.target.reset();
+  }
 }
 
 async function handMore(event) {
@@ -99,11 +86,9 @@ async function handMore(event) {
       page === Math.ceil(nextPage.totalHits / 15)
     ) {
       moreBtn.classList.add('visually-hidden');
-      setTimeout(() => {
-        iziToast.warning({
-          message: "We're sorry, but you've reached the end of search results.",
-        });
-      }, 2000);
+      iziToast.warning({
+        message: "We're sorry, but you've reached the end of search results.",
+      });
     }
 
     const item = document.querySelector('.gallery-item');
